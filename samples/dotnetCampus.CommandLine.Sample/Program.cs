6bd1096166d6b917f0625e5dfc91965e8862ad6b
walterlv <xblero@gmail.com>
2024-04-19 21:27:36 +0800

    排除老旧的项目

--- samples/dotnetCampus.CommandLine.Sample/Program.cs
+++ samples/dotnetCampus.CommandLine.Sample/Program.cs
@@ -1,17 +1,14 @@
-﻿using dotnetCampus.Cli.Properties;
-using dotnetCampus.Cli.Standard;
-
-namespace dotnetCampus.Cli
+﻿namespace dotnetCampus.Cli
 {
     class Program
     {
         static void Main(string[] args)
         {
-            CommandLine.Parse(args, LocalizableStrings.ResourceManager)
-                .AddStandardHandlers()
-                .AddHandler<DefaultOptions>(o => o.Run())
-                .AddHandler<SampleOptions>(o => o.Run())
-                .Run();
+            // CommandLine.Parse(args, LocalizableStrings.ResourceManager)
+            //     .AddStandardHandlers()
+            //     .AddHandler<DefaultOptions>(o => o.Run())
+            //     .AddHandler<SampleOptions>(o => o.Run())
+            //     .Run();
         }
     }
 }
