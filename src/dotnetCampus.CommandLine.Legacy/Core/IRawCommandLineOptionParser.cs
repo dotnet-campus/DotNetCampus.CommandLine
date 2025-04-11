397f865197ee1b5d157387992145516d0c312d0e
walterlv <xblero@gmail.com>
2025-04-11 15:59:23 +0800

    删除旧项目文件（虽然还有功能没有移植过来）

--- src/dotnetCampus.CommandLine.Legacy/Core/IRawCommandLineOptionParser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace dotnetCampus.Cli.Core
-{
-    internal interface IRawCommandLineOptionParser<out T>
-    {
-        void SetValue(char shortName, SingleOptimizedStrings? values);
-        void SetValue(string longName, SingleOptimizedStrings? values);
-    }
-}
--- src/dotnetCampus.CommandLine.Legacy/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("dotnetCampus.CommandLine.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
