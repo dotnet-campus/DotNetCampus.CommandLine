b1032dcf53e5653a5980cbbb423f35fe79826cce
walterlv <xblero@gmail.com>
2025-04-16 23:45:48 +0800

    给内部代码添加源生成器

---
+++ src/dotnetCampus.CommandLine.PrivateAnalyzer/Generators/ArgumentValueConverterGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace DotNetCampus.CommandLine.Generators;
+
+[Generator(LanguageNames.CSharp)]
+public class VerbCreatorGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(Execute);
+    }
+
+    private void Execute(IncrementalGeneratorPostInitializationContext context)
+    {
+    }
+}
