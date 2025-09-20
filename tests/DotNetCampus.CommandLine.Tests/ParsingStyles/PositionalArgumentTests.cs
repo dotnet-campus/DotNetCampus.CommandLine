e4fd14ff57064441bd086c4197401cf178c2e6d4
walterlv <xblero@gmail.com>
2025-09-20 20:57:57 +0800

    测试更多位置参数

--- tests/DotNetCampus.CommandLine.Tests/ParsingStyles/PositionalArgumentTests.cs
+++ tests/DotNetCampus.CommandLine.Tests/ParsingStyles/PositionalArgumentTests.cs
@@ -1,4 +1,6 @@
 using DotNetCampus.Cli.Compiler;
+using DotNetCampus.Cli.Exceptions;
+using DotNetCampus.Cli.Utils.Parsers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DotNetCampus.Cli.Tests.ParsingStyles;
@@ -15,6 +17,14 @@ public class PositionalArgumentTests
     [DataRow(new[] { "-o", "option", "value" }, TestCommandLineStyle.DotNet, DisplayName = "[DotNet] -o option value")]
     [DataRow(new[] { "-o", "option", "value" }, TestCommandLineStyle.Gnu, DisplayName = "[Gnu] -o option value")]
     [DataRow(new[] { "-o", "option", "value" }, TestCommandLineStyle.PowerShell, DisplayName = "[PowerShell] -o option value")]
+    [DataRow(new[] { "value", "-o", "option" }, TestCommandLineStyle.Flexible, DisplayName = "[Flexible] value -o option")]
+    [DataRow(new[] { "value", "-o", "option" }, TestCommandLineStyle.DotNet, DisplayName = "[DotNet] value -o option")]
+    [DataRow(new[] { "value", "-o", "option" }, TestCommandLineStyle.Gnu, DisplayName = "[Gnu] value -o option")]
+    [DataRow(new[] { "value", "-o", "option" }, TestCommandLineStyle.PowerShell, DisplayName = "[PowerShell] value -o option")]
+    [DataRow(new[] { "-o", "option", "--", "value" }, TestCommandLineStyle.Flexible, DisplayName = "[Flexible] -o option -- value")]
+    [DataRow(new[] { "-o", "option", "--", "value" }, TestCommandLineStyle.DotNet, DisplayName = "[DotNet] -o option -- value")]
+    [DataRow(new[] { "-o", "option", "--", "value" }, TestCommandLineStyle.Gnu, DisplayName = "[Gnu] -o option -- value")]
+    [DataRow(new[] { "test://value?option=option" }, TestCommandLineStyle.Url, DisplayName = "[Url] test://value?option=option")]
     public void Supported(string[] args, TestCommandLineStyle style)
     {
         // Arrange
@@ -25,6 +35,20 @@ public class PositionalArgumentTests
 
         // Assert
         Assert.AreEqual("value", options.Value);
+    }
+
+    [TestMethod]
+    [DataRow(new[] { "-o", "option", "--", "value" }, TestCommandLineStyle.PowerShell, DisplayName = "[PowerShell] -o option -- value")]
+    public void DoesNotSupportPostPositionalArguments(string[] args, TestCommandLineStyle style)
+    {
+        // Arrange
+        var commandLine = CommandLine.Parse(args, style.ToParsingOptions());
+
+        // Act
+        var exception = Assert.Throws<CommandLineParseException>(() => commandLine.As<TestOptions>());
+
+        // Assert
+        Assert.AreEqual(CommandLineParsingError.OptionalArgumentNotFound, exception.Reason);
     }
 
     public record TestOptions
