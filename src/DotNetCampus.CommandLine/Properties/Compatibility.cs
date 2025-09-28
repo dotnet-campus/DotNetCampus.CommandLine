5aa3631f34ed24ff7d673edf5723271216b5a698
walterlv <xblero@gmail.com>
2025-09-28 19:03:02 +0800

    升级库

--- src/DotNetCampus.CommandLine/Properties/Compatibility.cs
+++ src/DotNetCampus.CommandLine/Properties/Compatibility.cs
@@ -6,13 +6,6 @@ internal static class CompatibilityExtensionMethods
 {
 #if NETCOREAPP3_1_OR_GREATER
 #else
-    internal static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> pair, out TKey key, out TValue value)
-        where TKey : notnull
-    {
-        key = pair.Key;
-        value = pair.Value;
-    }
-
     internal static bool TryAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
         where TKey : notnull
     {
